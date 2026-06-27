efef217f65397ef702a661f91f1f9c420c179fc2
louisdesaintpaul67 <louisdesaintpaul@gmail.com>
2026-06-28 01:27:14 +0200

    Update index.ts

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
-
+import authRouter from "./auth";
+import adminRouter from "./admin";
+import dataRouter from "./data";
 const router: IRouter = Router();
-
 router.use(healthRouter);
-
+router.use(authRouter);
+router.use(adminRouter);
+router.use(dataRouter);
 export default router;
